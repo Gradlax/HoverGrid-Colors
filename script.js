1299c18e7a7c501925219f7a6b6043e70c49d503
Gradlax <valhama@protonmail.com>
2022-10-09 14:29:11 -0500

    theOneAndOnly

---
+++ script.js
@@ -0,0 +1,30 @@
+const container = document.getElementById('container')
+const colors = ['#d47d3d','#5ddc34','#2000df','#r4g4f4','#3215dd','#43ggge','#242323']
+const SQUARES = 650
+for(let i = 0; i < SQUARES; i++) {
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', () => setColor(square))
+
+    square.addEventListener('mouseout', () => removeColor(square))
+
+    container.appendChild(square)
+}
+
+function setColor(element) {
+   const color = getRandomColor()
+//    console.log(element)
+   element.style.background = color
+   element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`
+}
+
+function removeColor(element) {
+   element.style.background = '#1d1d1d'
+   element.style.boxShadow = '0 0 2px #000'
+}
+
+function getRandomColor() {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
+
